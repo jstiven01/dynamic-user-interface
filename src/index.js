c6950fb7bffb7ee0763573459f26302b13957cae
jstiven01 <stiven10@gmail.com>
2019-12-27 11:51:38 -0500

    working with async babel polyfill to use async in webpack

--- src/index.js
+++ src/index.js
@@ -34,3 +34,37 @@ const studentStringfy = JSON.stringify(student);
 console.log(typeof studentStringfy, studentStringfy);
 const JSONtoStudent = JSON.parse(studentStringfy);
 console.log(typeof JSONtoStudent, JSONtoStudent);
+
+// api img
+
+const img = document.getElementById('api-img');
+const img2 = document.getElementById('api-img2');
+console.log(img);
+const url = 'https://api.giphy.com/v1/gifs/translate?api_key=d6tnryBJfL052TAPNH5s0DRpGhI9EA8r&s=cats'
+
+//using fetch
+fetch(url, {mode: 'cors'}).then(function(response) {
+  return response.json();
+}).then(function(response) {
+  img.src = response.data.images.original.url;
+});
+
+async function getCats(){
+  /* No await 2 then
+  fetch(url, {mode: 'cors'})
+  .then((response) => {response.json()})
+  .then((response) => {
+    img2.src = response.data.images.original.url;
+  })*/
+  // first then into wait
+  /*
+  const response = await fetch(url, {mode: 'cors'})
+  response.json().then((response) => {
+    img2.src = response.data.images.original.url;
+  })*/
+  const response = await fetch(url, {mode: 'cors'});
+  const catData = await response.json();
+  img2.src = catData.data.images.original.url;
+}
+getCats();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  //entry: './src/index.js',
+  entry: {
+    app: ["babel-polyfill", './src/index.js']
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
